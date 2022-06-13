822f77906453b2cd447bc81525461b975ca643c2
emonShimoul <emonshimoul@gmail.com>
2022-06-13 11:54:31 +0600

    make responsive using css media query

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Service.css';
 
 const Service = ({service}) => {
     // const {service} = props;
     const {name, price, description, img} = service;
     return (
-        <div>
-            <img style={{width: 400, height:250}} src={img} alt="" />
+        <div className='service'>
+            <img style={{width: 500, height:300}} src={img} alt="" />
             <h3>{name}</h3>
             <h5>Price: {price}</h5>
             <p>{description}</p>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
-
-// https://ibb.co/86YVwj3
-// https://ibb.co/jkPH3J4
-// https://ibb.co/f0s1cc9
-// https://ibb.co/G2x6pn3
-// https://ibb.co/mRyJcvN
-// https://ibb.co/PQHQbKW
+import './Services.css';
 
 const Services = () => {
     const [services, setServices] = useState([]);
