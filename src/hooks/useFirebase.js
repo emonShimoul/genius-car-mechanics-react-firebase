5bb4221f14f7adcfda3341c80aa43798187f67e5
emonShimoul <emonshimoul@gmail.com>
2022-06-14 15:24:01 +0600

    set usefirebase function and onAuthStateChanged

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,40 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+const useFirebase = () => {
+    const [users, setUsers] = useState({});
+
+    const auth = getAuth();
+
+    const signInUsingGoogle = () => {
+        const googleProvider = new GoogleAuthProvider();
+        signInWithPopup(auth, googleProvider)
+        .then(result => {
+            setUsers(result.user);
+        });
+    }
+
+    // observe user state change
+    useEffect(() => {
+        const unSubscribed = onAuthStateChanged(auth, user => {
+            if(user){
+                setUsers(user);
+            }
+            else{
+                setUsers({});
+            }
+        });
+        return () => unSubscribed;
+    }, [])
+
+    const logOut = () => {
+        signOut(auth)
+        .then(()=>{});
+    }
+
+    return {
+        users,
+        signInUsingGoogle,
+        logOut
+    };
+}
