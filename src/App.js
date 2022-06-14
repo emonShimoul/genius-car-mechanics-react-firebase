1146b61b297e9e9f61d6d8b9f538edba07d82412
emonShimoul <emonshimoul@gmail.com>
2022-06-14 12:28:49 +0600

    basic router setup with 404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </Router>
     </div>
   );
 }
